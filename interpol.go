b9f7e2a9cea5a5517411ec2c53baf7d7e0ad24b6
Mário Freitas <imkira@gmail.com>
2016-09-19 03:28:56 +0900

    modified: allow template reader and output writer

--- interpol.go
+++ interpol.go
@@ -8,6 +8,7 @@ import (
 	"bytes"
 	"errors"
 	"io"
+	"strings"
 )
 
 // Errors returned when formatting templates.
@@ -15,6 +16,7 @@ var (
 	ErrUnexpectedClose = errors.New("interpol: unexpected close in template")
 	ErrExpectingClose  = errors.New("interpol: expecting close in template")
 	ErrKeyNotFound     = errors.New("interpol: key not found")
+	ErrReadByteFailed  = errors.New("interpol: read byte failed")
 )
 
 // Func receives the placeholder key and writes to the io.Writer. If an error
@@ -22,15 +24,135 @@ var (
 // will be aborted.
 type Func func(key string, w io.Writer) error
 
+// New creates a new interpolator with the given list of options.
+// You can use options such as the ones returned by WithTemplate, WithFormat
+// and WithOutput.
+func New(opts ...Option) *Interpolator {
+	opts2 := &Options{}
+	setOptions(opts, newOptionSetter(opts2))
+	return NewWithOptions(opts2)
+}
+
+// NewWithOptions creates a new interpolator with the given options.
+func NewWithOptions(opts *Options) *Interpolator {
+	return &Interpolator{
+		template: templateReader(opts),
+		output:   outputWriter(opts),
+		format:   opts.Format,
+		rb:       make([]rune, 0, 64),
+		start:    -1,
+		closing:  false,
+	}
+}
+
+// Interpolator interpolates Template to Output, according to Format.
+type Interpolator struct {
+	template io.RuneReader
+	output   runeWriter
+	format   Func
+	rb       []rune
+	start    int
+	closing  bool
+}
+
+// Interpolate reads runes from Template and writes them to Output, with the
+// exception of placeholders which are passed to Format.
+func (i *Interpolator) Interpolate() error {
+	for pos := 0; ; pos++ {
+		r, _, err := i.template.ReadRune()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return err
+		}
+		if err := i.parse(r, pos); err != nil {
+			return err
+		}
+	}
+	return i.finish()
+}
+
+func (i *Interpolator) parse(r rune, pos int) error {
+	switch r {
+	case '{':
+		return i.open(pos)
+	case '}':
+		return i.close()
+	default:
+		return i.append(r)
+	}
+}
+
+func (i *Interpolator) open(pos int) error {
+	if i.closing {
+		return ErrUnexpectedClose
+	}
+	if i.start >= 0 {
+		if _, err := i.output.WriteRune('{'); err != nil {
+			return err
+		}
+		i.start = -1
+	} else {
+		i.start = pos + 1
+	}
+	return nil
+}
+
+func (i *Interpolator) close() error {
+	if i.start >= 0 {
+		if err := i.format(string(i.rb), i.output); err != nil {
+			return err
+		}
+		i.rb = i.rb[:0]
+		i.start = -1
+	} else if i.closing {
+		i.closing = false
+		if _, err := i.output.WriteRune('}'); err != nil {
+			return err
+		}
+	} else {
+		i.closing = true
+	}
+	return nil
+}
+
+func (i *Interpolator) append(r rune) error {
+	if i.closing {
+		return ErrUnexpectedClose
+	}
+	if i.start < 0 {
+		_, err := i.output.WriteRune(r)
+		return err
+	}
+	i.rb = append(i.rb, r)
+	return nil
+}
+
+func (i *Interpolator) finish() error {
+	if i.start >= 0 {
+		return ErrExpectingClose
+	}
+	if i.closing {
+		return ErrUnexpectedClose
+	}
+	return nil
+}
+
 // WithFunc interpolates the specified template with replacements using the
 // given function.
 func WithFunc(template string, format Func) (string, error) {
-	f := newInterpolator(template)
-	f.format = format
-	if err := f.interpolate(); err != nil {
+	buffer := bytes.NewBuffer(make([]byte, 0, len(template)))
+	opts := &Options{
+		Template: strings.NewReader(template),
+		Output:   buffer,
+		Format:   format,
+	}
+	i := NewWithOptions(opts)
+	if err := i.Interpolate(); err != nil {
 		return "", err
 	}
-	return f.buffer.String(), nil
+	return buffer.String(), nil
 }
 
 // WithMap interpolates the specified template with replacements using the
@@ -46,89 +168,4 @@ func WithMap(template string, m map[string]string) (string, error) {
 		return err
 	}
 	return WithFunc(template, format)
-}
-
-type interpolator struct {
-	template string
-	buffer   *bytes.Buffer
-	format   Func
-	start    int
-	closing  bool
-}
-
-func newInterpolator(template string) *interpolator {
-	buffer := bytes.NewBuffer(nil)
-	buffer.Grow(len(template))
-	return &interpolator{
-		template: template,
-		buffer:   buffer,
-		start:    -1,
-		closing:  false,
-	}
-}
-
-func (f *interpolator) open(i int) error {
-	if f.closing {
-		return ErrUnexpectedClose
-	}
-	if f.start >= 0 {
-		f.buffer.WriteRune('{')
-		f.start = -1
-	} else {
-		f.start = i + 1
-	}
-	return nil
-}
-
-func (f *interpolator) close(i int) error {
-	if f.start >= 0 {
-		if err := f.format(f.template[f.start:i], f.buffer); err != nil {
-			return err
-		}
-		f.start = -1
-	} else if f.closing {
-		f.closing = false
-	} else {
-		f.closing = true
-		f.buffer.WriteRune('}')
-	}
-	return nil
-}
-
-func (f *interpolator) interpolate() error {
-	var err error
-	for i, t := range f.template {
-		switch t {
-		case '{':
-			err = f.open(i)
-		case '}':
-			err = f.close(i)
-		default:
-			err = f.append(t)
-		}
-		if err != nil {
-			return err
-		}
-	}
-	return f.finish()
-}
-
-func (f *interpolator) finish() error {
-	if f.start >= 0 {
-		return ErrExpectingClose
-	}
-	if f.closing {
-		return ErrUnexpectedClose
-	}
-	return nil
-}
-
-func (f *interpolator) append(t rune) error {
-	if f.closing {
-		return ErrUnexpectedClose
-	}
-	if f.start < 0 {
-		f.buffer.WriteRune(t)
-	}
-	return nil
 }
--- interpol_test.go
+++ interpol_test.go
@@ -1,15 +1,190 @@
 package interpol
 
 import (
+	"bytes"
 	"errors"
 	"io"
 	"strings"
 	"testing"
 )
 
+var errDummy = errors.New("dummy")
+
+func toError(key string, w io.Writer) error {
+	return errDummy
+}
+
 func toUpper(key string, w io.Writer) error {
-	w.Write([]byte(strings.ToUpper(key)))
+	_, err := w.Write([]byte(strings.ToUpper(key)))
+	return err
+}
+
+func toNull(key string, w io.Writer) error {
 	return nil
+}
+
+func testInterpolate(template string, f Func) (string, error) {
+	buffer := bytes.NewBuffer(nil)
+	opts := &Options{
+		Template: strings.NewReader(template),
+		Output:   buffer,
+		Format:   f,
+	}
+	i := NewWithOptions(opts)
+	err := i.Interpolate()
+	return buffer.String(), err
+}
+
+func TestInterpolatorEscapeOpen(t *testing.T) {
+	str, err := testInterpolate("hello {{!!!{{", toUpper)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if str != "hello {!!!{" {
+		t.Errorf("Invalid string: %q", str)
+	}
+}
+
+func TestInterpolatorEscapeClose(t *testing.T) {
+	str, err := testInterpolate("hello }}!!!}}", toUpper)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if str != "hello }!!!}" {
+		t.Errorf("Invalid string: %q", str)
+	}
+}
+
+func TestInterpolatorEscapeOpenClose(t *testing.T) {
+	str, err := testInterpolate("hello }}!!!{{", toUpper)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if str != "hello }!!!{" {
+		t.Errorf("Invalid string: %q", str)
+	}
+}
+
+func testInterpolatorClose(t *testing.T, strs []string, err error) {
+	for _, str := range strs {
+		strs2 := strings.Split(str, ":")
+		template, output := strs2[0], strs2[1]
+		output2, err2 := testInterpolate(template, toUpper)
+		if err2 != err {
+			t.Fatal(err2)
+		}
+		if output != output2 {
+			t.Fatalf("Expecting %q but got %q", output, output2)
+		}
+	}
+}
+
+func TestInterpolatorUnexpectedClose(t *testing.T) {
+	strs := []string{
+		"}hello test!!!:",
+		"hello test}!!!:hello test",
+		"hello test!!!}:hello test!!!",
+		"hello test!!!}{:hello test!!!",
+	}
+	testInterpolatorClose(t, strs, ErrUnexpectedClose)
+}
+
+func TestInterpolatorExpectingClose(t *testing.T) {
+	strs := []string{
+		"{hello test!!!:",
+		"hello {test!!!:hello ",
+		"hello test!!!{:hello test!!!",
+	}
+	testInterpolatorClose(t, strs, ErrExpectingClose)
+}
+
+type testIOFunc func(t *testing.T, template, output string)
+
+func testInterpolateIOError(t *testing.T, f testIOFunc) {
+	strs := []string{
+		"こんにちは:こんにち",
+		"こんにちは:こんに",
+		"こんにちは:こん",
+		"こんにちは:こ",
+		"こんにちは:",
+		"hello test!!!:",
+		"hello test!!!:h",
+		"hello test!!!:hello test!!",
+		"{{hello test!!!:",
+		"hello {{test!!!:hello ",
+		"hello test!!!{{:hello test!!!",
+		"}}hello test!!!:",
+		"hello }}test!!!:hello ",
+		"hello test!!!}}:hello test!!!",
+	}
+	for _, str := range strs {
+		strs2 := strings.Split(str, ":")
+		template, output := strs2[0], strs2[1]
+		f(t, template, output)
+	}
+}
+
+func testInterpolateReadError(t *testing.T, template, output string) {
+	reader := newErrReader(strings.NewReader(template))
+	reader.xn = len(output)
+	reader.err = errDummy
+	buffer := bytes.NewBuffer(nil)
+	opts := &Options{
+		Template: reader,
+		Output:   buffer,
+		Format:   toError,
+	}
+	i := NewWithOptions(opts)
+	err := i.Interpolate()
+	if err != errDummy {
+		t.Fatalf("Expecting %v got %v for %q", errDummy, err, template)
+	}
+	if str := buffer.String(); str != output {
+		t.Fatalf("Expecting %q but got %q for %q", output, str, template)
+	}
+}
+
+func TestInterpolatorReadError(t *testing.T) {
+	testInterpolateIOError(t, testInterpolateReadError)
+}
+
+func testInterpolateWriteError(t *testing.T, template, output string) {
+	writer := newErrWriter()
+	writer.xn = len(output)
+	writer.err = errDummy
+	opts := &Options{
+		Template: strings.NewReader(template),
+		Output:   writer,
+		Format:   toUpper,
+	}
+	i := NewWithOptions(opts)
+	err := i.Interpolate()
+	if err != errDummy {
+		t.Fatalf("Expecting %v got %v for %q", errDummy, err, template)
+	}
+	if str := writer.buf.String(); str != output {
+		t.Fatalf("Expecting %q but got %q for %q", output, str, template)
+	}
+}
+
+func TestInterpolatorWriteError(t *testing.T) {
+	testInterpolateIOError(t, testInterpolateWriteError)
+}
+
+func TestInterpolatorNoRuneWriter(t *testing.T) {
+	reader := strings.NewReader("{hello} {世界}!")
+	template := WithTemplate(reader)
+	format := WithFormat(toUpper)
+	w := newErrWriter()
+	w.xn = reader.Len()
+	output := WithOutput(w)
+	i := New(template, format, output)
+	if err := i.Interpolate(); err != nil {
+		t.Fatal(err)
+	}
+	if str := w.buf.String(); str != "HELLO 世界!" {
+		t.Fatalf("Got %q", str)
+	}
 }
 
 func TestWithFunc(t *testing.T) {
@@ -22,72 +197,9 @@ func TestWithFunc(t *testing.T) {
 	}
 }
 
-func TestWithFuncEscapeOpen(t *testing.T) {
-	str, err := WithFunc("hello {{!!!{{", toUpper)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if str != "hello {!!!{" {
-		t.Errorf("Invalid string: %q", str)
-	}
-}
-
-func TestWithFuncEscapeClose(t *testing.T) {
-	str, err := WithFunc("hello }}!!!}}", toUpper)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if str != "hello }!!!}" {
-		t.Errorf("Invalid string: %q", str)
-	}
-}
-
-func TestWithFuncEscapeOpenClose(t *testing.T) {
-	str, err := WithFunc("hello }}!!!{{", toUpper)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if str != "hello }!!!{" {
-		t.Errorf("Invalid string: %q", str)
-	}
-}
-
-func TestWithFuncUnexpectedClose(t *testing.T) {
-	strs := []string{
-		"}hello test!!!",
-		"hello test}!!!",
-		"hello test!!!}",
-		"hello test!!!}{",
-	}
-	for _, s := range strs {
-		str, err := WithFunc(s, toUpper)
-		if len(str) != 0 || err != ErrUnexpectedClose {
-			t.Fatal(err)
-		}
-	}
-}
-
-func TestWithFuncExpectingClose(t *testing.T) {
-	strs := []string{
-		"{hello test!!!",
-		"hello {test!!!",
-		"hello test!!!{",
-	}
-	for _, s := range strs {
-		str, err := WithFunc(s, toUpper)
-		if len(str) != 0 || err != ErrExpectingClose {
-			t.Fatal(err)
-		}
-	}
-}
-
 func TestWithFuncFuncError(t *testing.T) {
-	dummy := errors.New("dummy")
-	f := func(key string, w io.Writer) error {
-		return dummy
-	}
-	str, err := WithFunc("hello {test}!!!", f)
-	if len(str) != 0 || err != dummy {
+	str, err := WithFunc("hello {test}!!!", toError)
+	if len(str) != 0 || err != errDummy {
 		t.Fatal(err)
 	}
 }
@@ -120,5 +232,33 @@ func TestWithMapNil(t *testing.T) {
 	str, err := WithMap("hello {test}!!!", nil)
 	if len(str) != 0 || err != ErrKeyNotFound {
 		t.Fatal(err)
+	}
+}
+
+func BenchmarkWithFunc0(b *testing.B) {
+	benchmarkWithFuncN(b, 0)
+}
+
+func BenchmarkWithFunc1(b *testing.B) {
+	benchmarkWithFuncN(b, 1)
+}
+
+func BenchmarkWithFunc1K(b *testing.B) {
+	benchmarkWithFuncN(b, 1000)
+}
+
+func benchmarkWithFuncN(b *testing.B, n int) {
+	b.StopTimer()
+	template := strings.Repeat("?{hello}!", n)
+	expected := strings.Repeat("?!", n)
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		str, err := WithFunc(template, toNull)
+		if err != nil {
+			b.Fatal(err)
+		}
+		if str != expected {
+			b.Fatalf("Invalid")
+		}
 	}
 }
